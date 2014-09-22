e53a7dd0a5bcb324bd000ebc2657a4a691cb9385
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-09-22 12:56:52 +0600

    Created the base rails backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # if someone asks for html, redirect them to the home page, we only serve json
+  def intercept_html_requests
+    redirect_to '/' if request.format == Mime::HTML
+  end
 end
